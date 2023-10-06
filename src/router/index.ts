2bfbfa0babf9292f45eb7c3f6dd7abc1caaae03e
Ezequiel Dias <ezequidias@outlook.com>
2023-10-06 13:56:09 -0300

    configure routes

---
+++ src/router/index.ts
@@ -0,0 +1,39 @@
+import { useAuthStore } from '@/stores';
+import { setupLayouts } from 'virtual:generated-layouts';
+import { createRouter, createWebHistory } from 'vue-router';
+import routes from '~pages';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    ...setupLayouts(routes),
+  ],
+  scrollBehavior() {
+    return { top: 0 }
+  },
+})
+
+router.beforeEach(async (to, from) => {
+   // redirect to login page if not logged in and trying to access a restricted page
+    const publicPages = ['/login', '/register'];
+    const authRequired = !publicPages.includes(to.path);
+    const auth = useAuthStore();
+    const token = localStorage.getItem(import.meta.env.VITE_TOKEN_NAME)
+
+    if(!auth.user && token){
+      try{
+        const user_details = await auth.$axios.get('user')
+        auth.status.loggedIn = true
+        auth.user = user_details
+        if(!authRequired) return '/'
+      }catch(e){
+        if(!authRequired) return true
+        return '/login'
+      }
+    }
+
+    if (authRequired && !auth.status.loggedIn) return '/login';
+    return true;
+})
+
+export default router
